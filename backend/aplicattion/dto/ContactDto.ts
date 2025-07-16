6a4834ce4bd9c767d75446d161bc64b272c27ced
MateusMonteiroDV <mateusdosantosmonteiro12@gmail.com>
2025-07-15 22:00:33 -0300

    adding_user_and_contact_user_entities

---
+++ backend/aplicattion/dto/ContactDto.ts
@@ -0,0 +1,7 @@
+export interface ContactOwnerDto{
+	 readonly id:string,
+	 readonly id_user:string,
+	 phone_number:string,
+
+}
+
---
+++ backend/aplicattion/dto/UserDto.ts
@@ -0,0 +1,32 @@
+
+
+export interface UserOwnerDto{
+	readonly id:string;
+	name:string;
+	email:string;
+	password:string;
+
+
+
+
+}
+
+
+export interface CreateUserOwnerDto {
+		readonly id:string;
+		name:string;
+		email:string;
+		password:string;
+		
+
+
+}
+
+export interface UserOwnerResponseDto{
+        name:string;
+        phoneNumber:string;
+
+
+}
+
+i
