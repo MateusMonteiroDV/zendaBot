401eab0e7b25a21bcc6ecf25462bfe51fab2195e
MateusMonteiroDV <mateusdosantosmonteiro12@gmail.com>
2025-08-19 22:00:04 -0300

    adding_chatApiAdapter_from_api_groq

--- backend/container.ts
+++ backend/container.ts
@@ -1,26 +1,31 @@
-import  dotenv from 'dotenv';
+import dotenv from 'dotenv';
 
-dotenv.config({path:'./.env'})
+dotenv.config({ path: './.env' })
 
 
 
-import {TokenJWT} from './infrastructure/tokenJWT.js'
-import {UserRepository} from './infrastructure/model/UserRepository.js'
-import {RegisterUserCase} from './aplicattion/use-cases/User/RegisterUserCase.js';
-import {LoginUserCase} from './aplicattion/use-cases/User/LoginUserCase.js';
-import {UserController} from './router/user/UserController.js';
+import { TokenJWT } from './infrastructure/tokenJWT.js'
+import { UserRepository } from './infrastructure/model/UserRepository.js'
+import { RegisterUserCase } from './aplicattion/use-cases/User/RegisterUserCase.js';
+import { LoginUserCase } from './aplicattion/use-cases/User/LoginUserCase.js';
+import { UserController } from './router/user/UserController.js';
+import { ChatApiAdapter } from './infrastructure/ChatGptApiAdapter.js';
+import Groq from 'groq-sdk';
 
-let tokenJWT:TokenJWT = new TokenJWT(process.env.JWT_SECRET_KEY || '1234', process.env.JWT_EXSPIRES_TIME || '5hr');
+
+let tokenJWT: TokenJWT = new TokenJWT(process.env.JWT_SECRET_KEY || '1234', process.env.JWT_EXSPIRES_TIME || '5hr');
 let userRepository = new UserRepository();
 
-let registerUserCase:RegisterUserCase = new RegisterUserCase(userRepository,tokenJWT);
-let loginUserCase:LoginUserCase  = new LoginUserCase(userRepository,tokenJWT)
+let registerUserCase: RegisterUserCase = new RegisterUserCase(userRepository, tokenJWT);
+let loginUserCase: LoginUserCase = new LoginUserCase(userRepository, tokenJWT)
 
+let groq: Groq = new Groq();
+let chatApiAdapter: ChatApiAdapter = new ChatApiAdapter(groq)
 
-let userController:UserController = new UserController(registerUserCase, loginUserCase);
+let userController: UserController = new UserController(registerUserCase, loginUserCase);
 
 export const container = {
-	 userController	
+  userController
 
 
 }
--- backend/infrastructure/ChatGptApiAdapter.ts
+++ backend/infrastructure/ChatGptApiAdapter.ts
@@ -1,10 +1,23 @@
 import { IChatApiAdapter } from '../repository/IChatApiAdapter'
 import { ChatMessageDto } from '../aplicattion/dto/UserDto.js'
+import Groq from 'groq-sdk'
 
 export class ChatApiAdapter implements IChatApiAdapter {
+  constructor(private grogApiService: Groq) { }
+
+
   async reply(message: ChatMessageDto) {
+    const response = await this.grogApiService.chat.completions.create({
+      messages: [
+        {
+          role: "user",
+          content: "Explain the importance of fast language models",
+        },
+      ],
+      model: "llama-3.3-70b-versatile",
+    });
 
+    const text: string | null = response.choices[0].message.content
+    return text
   }
-
-
 }
