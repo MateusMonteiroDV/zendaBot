da379f84522be6117e1fba79748a27d8f064081b
MateusMonteiroDV <mateusdosantosmonteiro12@gmail.com>
2025-09-12 20:10:08 -0300

    arrange_some_codes

--- backend/aplicattion/dto/UserDto.ts
+++ backend/aplicattion/dto/UserDto.ts
@@ -25,44 +25,25 @@ export interface UserOwnerDb {
   created_at: Date;
 }
 
-
-
 export interface UserOwnerDto {
   id: string;
   name: string;
   email: string;
   password: string;
-
-
-
-
 }
-
-
 export interface UserOwnerRegisterInputDto {
   name: string;
   email: string;
   password: string;
-
-
-
 }
-
 export interface UserOwnerLoginInputDto {
-
   email: string;
   password: string;
-
-
-
 }
-
-
 export type UserOwnerRegiserOutputDto = string
 
 export interface UserOwnerResponseDto {
   id: string
-
 }
 
 
