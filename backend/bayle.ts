ef8343317cc28e0fc7907e7f43c8926f0085e37c
MateusMonteiroDV <mateusdosantosmonteiro12@gmail.com>
2025-09-30 22:31:19 -0300

    solved_bug_disconnect_faile_socket_whatssap

--- backend/bayle.ts
+++ backend/bayle.ts
@@ -1,16 +1,12 @@
 import makeWASocket, {
-  Browsers,
   useMultiFileAuthState,
   DisconnectReason,
-  WAMessage,
-  fetchLatestWaWebVersion,
-} from "baileys";
-import { Boom } from "@hapi/boom";
-import QRCode from "qrcode";
+  Browsers,
+} from "@whiskeysockets/baileys";
 
 import qrcode from "qrcode-terminal";
-
-//@ts-ignore
+import { Boom } from "@hapi/boom";
+import fs from "fs-extra"; // npm install fs-extra
 
 export async function startEventWhatssap() {
   const { state, saveCreds } = await useMultiFileAuthState("auth");
@@ -23,19 +19,41 @@ export async function startEventWhatssap() {
   });
 
   sock.ev.on("creds.update", saveCreds);
+
   sock.ev.on("connection.update", async (update) => {
     const { connection, lastDisconnect, qr } = update;
-    if (
-      connection === "close" &&
-      (lastDisconnect?.error as Boom)?.output?.statusCode ===
-        DisconnectReason.restartRequired
-    ) {
-      console.log("Restart required, restarting...");
-      startEventWhatssap();
+
+    if (connection === "close") {
+      const statusCode = (lastDisconnect?.error as Boom)?.output?.statusCode;
+
+      console.log("Connection closed, reason:", statusCode);
+
+      const shouldReconnect = statusCode !== DisconnectReason.loggedOut;
+
+      if (statusCode === DisconnectReason.loggedOut) {
+        console.log("Session logged out. Clearing saved auth...");
+
+        await fs.remove("auth");
+
+        startEventWhatssap();
+      }
+
+      if (shouldReconnect) {
+        startEventWhatssap();
+      }
+    } else if (connection === "open") {
+      console.log("✅ WhatsApp connection opened");
     }
 
     if (qr) {
       qrcode.generate(qr, { small: true });
+    }
+  });
+
+  sock.ev.on("messages.upsert", (m) => {
+    if (m.messages && m.messages.length > 0) {
+      const msg = m.messages[0];
+      console.log("Message Received: ", msg.message);
     }
   });
 }
